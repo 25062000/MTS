0fdcc420a52acd1b7812e5446d0f47b9b954311c
sivapriya256 <sivapriyaece256@gmail.com>
2024-04-16 17:32:03 +0530

    Second commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators,AbstractControl, ValidationErrors, ValidatorFn, } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor( private formBuilder: FormBuilder){}
+
+  submitted = false;
+  loginForm!: FormGroup;
+
+  ngOnInit(){
+  
+    this.loginForm = this.formBuilder.group({
+      
+      email: ['', [Validators.required, Validators.pattern(/^\s*\S+@\S+\.\S+\s*$/)]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]],   
+    }
+
+  );
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+
+  onSubmit(){
+
+    this.submitted = true;
+    console.log("hii")
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,73 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators,AbstractControl, ValidationErrors, ValidatorFn, } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule,
+     CommonModule,
+    ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  constructor( private formBuilder: FormBuilder){}
+
+
+  registerForm!: FormGroup;
+  submitted = false;
+
+  ngOnInit(){
+  
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(1),
+        Validators.maxLength(60), Validators.pattern(/^[a-zA-Z ]*$/)
+        ]],   
+      email: ['', [Validators.required, Validators.pattern(/^\s*\S+@\S+\.\S+\s*$/)]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]],
+      // confirmPassword: ['',[Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),]],
+      terms: ['',[Validators.required]],
+    
+    }
+
+  );
+  }
+
+  // confirmPasswordValidator(): ValidatorFn {
+  //   return (control: AbstractControl): ValidationErrors | null => {
+  //     const password = this.registerForm.get('password')?.value;
+  //     const confirmPassword = control.value;
+      
+  //     // Check if passwords match
+  //     const passwordsMatch = password === confirmPassword;
+
+  //     // If passwords don't match, return the error message
+  //     return passwordsMatch ? null : { PasswordNoMatch: "error" };
+  //   };
+  // }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit(){
+    
+    this.submitted = true;
+
+    if (this.registerForm.invalid){
+      return 
+    }
+
+
+    console.log("On submit function is executed");
+  }
+
+  // ConfirmedValidator(){
+
+  // }
+
+ 
 
 }
